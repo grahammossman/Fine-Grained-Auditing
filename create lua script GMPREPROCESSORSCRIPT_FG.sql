6cf066e0f71e551f8796134083eb0a39db8e21b9
grahammossman <grahammossman@gmail.com>
2016-04-14 17:45:13 +0100

    2016-04-14 Dealing with upper case
    Previous version just converted everything to upper case - this is more
    subtle !

--- create lua script GMPREPROCESSORSCRIPT_FG.sql
+++ create lua script GMPREPROCESSORSCRIPT_FG.sql
@@ -5,6 +5,7 @@ function gm_preprocessor_function_fg(sqltext)
 -- SQL must start with SELECT or CREATE ...VIEW to be eligible (optionally with leading spaces)
 -- otherwise return the original SQL
 --
+
 if ((string.find(string.upper(sqltext), '^%s*SELECT'))==nil 
 and (string.find(string.upper(sqltext), '^%s*CREATE%s+.*VIEW'))==nil) 
 then return sqltext
@@ -18,22 +19,38 @@ local conversions=query([[select table_name, altered_table_name from FG_SECURITY
 --
 if (#conversions==0) then return sqltext end
 --
--- Strings set to upper case (for simplicity)
---
-local returntext=string.upper(sqltext)
+
+returntext=sqltext
+
 --
 -- Loop round the table names that need replacing
 --
-for i=1,#conversions do
 
+for i=1,#conversions do
 this_table_name=conversions[i][1]
 this_replacement=conversions[i][2]
+
+--
+
+      local tokens = sqlparsing.tokenize(returntext)
+      for i=1,#tokens do
+			if string.upper(tokens[i]) == 'FROM' then
+				tokens[i]='FROM'
+			end 
+            if string.upper(tokens[i]) == 'OLAP.'..this_table_name then
+                        tokens[i] = string.upper(tokens[i])
+            end
+        end
+
+	returntext=table.concat(tokens)
+
 
 -- replacements are only relevant in a JOIN (INNER JOIN, OUTER JOIN ... etc.) 
 -- or in a FROM clause
 
-from_clause_regexp='FROM%s+(["]*[OLAP]*["]*[.]*["]*'..this_table_name..'["]*'
-join_clause_regexp='JOIN%s+(["]*[OLAP]*["]*[.]*["]*'..this_table_name..'["]*'
+
+from_clause_regexp='FROM%s+["]*[OLAP]*["]*[.]*["]*'..this_table_name..'["]*'
+join_clause_regexp='JOIN%s+["]*[OLAP]*["]*[.]*["]*'..this_table_name..'["]*'
 from_clause_replacement='FROM '..this_replacement..' '
 join_clause_replacement='JOIN '..this_replacement..' '
 returntext=string.gsub(returntext,from_clause_regexp,from_clause_replacement)
